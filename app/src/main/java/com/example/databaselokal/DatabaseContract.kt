bbbd121d8ff0e8e96fd89d83777aaa888f649c31
Lukman Nur Hakim <lukmannur@students.usu.ac.id>
2024-11-28 17:03:44 +0700

    apdet
    add homewrok
    homweorkadapter

--- app/src/main/java/com/example/databaselokal/DatabaseContract.kt
+++ app/src/main/java/com/example/databaselokal/DatabaseContract.kt
@@ -1,9 +1,12 @@
 package com.example.databaselokal
 
+import android.provider.BaseColumns
+
 internal class DatabaseContract {
-    internal class HomeworkColumns {
+    internal class HomeworkColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "homework"
+            const val ID = "_id"
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
