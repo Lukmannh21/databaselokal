b2f539b6cbedfc4c03dd528b34723e2b6ea5c429
briyanyehezkhiel <briyanyehezkhiel@gmail.com>
2024-11-28 18:38:14 +0700

    perbaikan  AddHomework,  DatabaseContract, MappingHelper

--- app/src/main/java/com/example/databaselokal/DatabaseContract.kt
+++ app/src/main/java/com/example/databaselokal/DatabaseContract.kt
@@ -6,7 +6,7 @@ internal class DatabaseContract {
     internal class HomeworkColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "homework"
-            const val ID = "_id"
+            const val _ID = "_id"
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
